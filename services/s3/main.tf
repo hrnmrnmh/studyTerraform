9f3b1f2f699ab329f28fe5d06755725fa9110e18
aohno <naka2357@gmail.com>
2023-07-06 12:04:16 +0900

    terraform s3 mime-type

--- services/s3/main.tf
+++ services/s3/main.tf
@@ -3,10 +3,10 @@ resource aws_s3_bucket bucket {
 }
 
 resource aws_s3_object object {
-  for_each = fileset("./services/s3/uploads/", "*")
-  bucket   = aws_s3_bucket.bucket.id
-  key = "/${each.value}"
-  source = "./services/s3/uploads/${each.value}"
-  etag = filemd5("./services/s3/uploads/${each.value}")
-  content_type = "text/html"
+  for_each     = fileset("./services/s3/uploads/", "*")
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "/${each.value}"
+  source       = "./services/s3/uploads/${each.value}"
+  etag         = filemd5("./services/s3/uploads/${each.value}")
+  content_type = endswith(each.value, ".html") ? "text/html" : endswith(each.value, ".js") ? "text/javascript" : endswith(each.value, ".css") ? "text/css" : "text/plain"
 }
